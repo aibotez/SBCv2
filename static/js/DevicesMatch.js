e09470d13efe0c267b5ac6f2aa54a4888ee1ea19
aibotez <2290227486@qq.com>
2022-02-21 23:12:25 +0800

    yoga

--- static/js/DevicesMatch.js
+++ static/js/DevicesMatch.js
@@ -1,13 +1,29 @@
 
 
+
+Match();
 function Match()
 {
-	var ScWidth = document.body.clientWidth;
-	var ScHeight = document.body.clientHeight;
 	
+	//alert(document.documentElement.style.fontSize);
+	var ScWidth = document.documentElement.clientWidth;
+	var ScHeight = document.documentElement.clientHeight;
+
+
 	if(ScHeight>ScWidth)
 	{
+		//竖版
+		document.documentElement.style.fontSize = ScWidth/60+'px';
+		document.getElementById("div1").style.display="none";
+		document.getElementById("HightModeMenu").style.display="";
+		document.getElementById("ShowMain").style.width = "100%";
 		
+	}
+	else
+	{
+		document.documentElement.style.fontSize = ScWidth/90+'px';
+		document.getElementById("div1").style.display="";
+		document.getElementById("HightModeMenu").style.display="none";
 	}
 	
 }
