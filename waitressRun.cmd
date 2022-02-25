f8a8987aaca92cb0c63c0eaf5594653069c79d9d
aibotez <2290227486@qq.com>
2022-02-25 19:52:42 +0800

    asipp

--- waitressRun.cmd
+++ waitressRun.cmd
@@ -1,1 +1,6 @@
-waitress-serve --port=90 SBC.wsgi:application
+@echo off
+mode con lines=30 cols=60
+%1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
+cd /d "%~dp0"
+rem 下面可以写你的bat代码了
+waitress-serve --port=90 --threads=4 SBC.wsgi:application
