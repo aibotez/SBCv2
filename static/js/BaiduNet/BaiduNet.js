4ee829ea521214adf77956913d035e295cf2efc7
aibotez <2290227486@qq.com>
2022-03-19 17:53:12 +0800

    asipp

--- static/js/BaiduNet/BaiduNet.js
+++ static/js/BaiduNet/BaiduNet.js
@@ -8,7 +8,12 @@ function BaiduNetShow()
 	console.log(res);
 	if(res.errno == '404')
 	{
-		var usercookie=prompt("输入cookie","");
+		var usercookie=prompt("输入cookie;直接确认会下载获取cookie工具，可通过该工具获取cookie","");
+		if (usercookie == "")
+		{
+			DownGetCookieTool();
+			return;
+		}
 		if (usercookie!=null && usercookie!="")
 		{
 			var urlpath = '/BaiduNetSaveUser/'
@@ -28,4 +33,14 @@ function BaiduNetShow()
 		//BaiduNetRefreshFile({'id':Showpath});
 		window.location.href="/BaiduNetHome/?showpath=/";
 	}
+}
+
+function DownGetCookieTool()
+{
+	downurl = "/static/Tools/GetCookie/GetCookie.exe";
+	let a = document.createElement('a');
+	let filename = "GetCookie.exe";
+	a.href = downurl;
+	a.click();
+	a.remove();
 }
