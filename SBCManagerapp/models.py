7275c60c04e3760753097b27d594c46efa87d5ce
aibotez <2290227486@qq.com>
2023-02-17 14:39:18 +0800

    refl

--- SBCManagerapp/models.py
+++ SBCManagerapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class SBCManager(models.Model):
+    FileStock = models.CharField(max_length=256)
+    UserStock = models.CharField(max_length=256)
+    SBCStockSize = models.IntegerField()
+    SBCUser0 = models.CharField(max_length=20)
+    SBCUserPass0 = models.CharField(max_length=20)
+    SBCManageEmail = models.CharField(max_length=30)
+    par1 = models.CharField(max_length=256)
+    par2 = models.CharField(max_length=256)
+    par3 = models.CharField(max_length=256)
--- SBCManagerapp/views.py
+++ SBCManagerapp/views.py
@@ -5,8 +5,42 @@ from django.contrib.auth.decorators import login_required
 from SBC import LoginVerfiy
 from django.http import HttpResponse,JsonResponse
 from django.http import HttpResponseRedirect
+from SBCManagerapp import models as SBCManagemodels
+from UserFileRecordapp import models as UserFileRecordmodels
 # Create your views here.
+from Usersapp.models import User
+from django.contrib.auth import authenticate,login,logout
 
+
+
+def size_format(size):
+    if size < 1024:
+        return '%i' % size + 'B'
+    elif 1024 <= size < 1024*1024:
+        return '%.1f' % float(size/1024) + 'KB'
+    elif 1024*1024 <= size < 1024*1024*1024:
+        return '%.1f' % float(size/(1024*1024)) + 'MB'
+    elif 1024*1024*1024 <= size < 1024*1024*1024*1024:
+        return '%.1f' % float(size/(1024*1024*1024)) + 'GB'
+    elif 1024*1024*1024*1024 <= size:
+        return '%.1f' % float(size/(1024*1024*1024*1024)) + 'TB'
 
 def sbcmanger(request):
+    Users = User.objects.all()
+    UserInfo = []
+    j = 1
+    for i in Users:
+        userinfo = {}
+        userinfo['username'] = i.username
+        userinfo['id'] = i.id
+        userinfo['numid'] = str(j)
+        userinfo['usedcapacity'] = i.usedcapacity
+        userinfo['totalcapacity'] = i.totalcapacity
+        userinfo['is_superuser'] = i.is_superuser
+        userinfo['usercapacityper'] = str(round((i.usedcapacity / i.totalcapacity) * 100,1))+'%'
+        userinfo['usercapacityinfo'] = '{}/{}  ({})'.format(size_format(i.usedcapacity),size_format(i.totalcapacity),userinfo['usercapacityper'])
+
+        UserInfo.append(userinfo)
+        j += 1
+
     return render(request, "SBCManager/sbcmanager.html", locals())
