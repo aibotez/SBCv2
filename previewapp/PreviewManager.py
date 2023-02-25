32412f6d5ef452218cdf3873a466719c565c07b2
aibotez <2290227486@qq.com>
2023-02-25 16:19:30 +0800

    refl

--- previewapp/PreviewManager.py
+++ previewapp/PreviewManager.py
@@ -123,4 +123,5 @@ class Preview():
             else:
                 VideoFrams = self.VideoFrams.GetVideoFrams(path,req['VideoFram'])
                 AudioFrams = self.VideoFrams.GetAudioFrams(AudioPath, req['AudioFram'])
+                print(type(VideoFrams),type(AudioFrams))
                 return {'res': 1, 'VideoFrams': VideoFrams,'AudioFrams':AudioFrams}
